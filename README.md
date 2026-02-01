845e3e33960318d8a65a61b867ce44f38f51c613
Rogelio Gungon <rogelio.gungon@paymaya.com>
2026-02-01 16:45:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mini Deposit Service
+
+A production-inspired digital banking deposit microservice built with Java, Spring Boot, Kafka, and PostgreSQL. This project demonstrates real-world patterns for financial transaction orchestration and event-driven system design.
+
+## Tech Stack
+- Java 21 (LTS)
+- Spring Boot 4.x
+- Apache Kafka
+- PostgreSQL
---
+++ src/main/java/com/rog/mini_deposit_service/MiniDepositServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rog.mini_deposit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDepositServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDepositServiceApplication.class, args);
+	}
+
+}
